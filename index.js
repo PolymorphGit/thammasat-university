6a9d992a690f52bb3341162a4f3eb14cd218803d
iyingac <iyingac@gmail.com>
2017-03-14 14:24:17 +0700

    Test Node js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var express = require('express')
-//var bodyParser = require('body-parser'), compression = require('compression'), cors = require('cors');
 var	account = require('./server/account')
 var app = express()
-/*
-var pg = require('pg'),databaseURL = process.env.DATABASE_URL || 'postgres://localhost:5432/df3pgi81qfmoc7';
-var client = new pg.Client(databaseURL);
-client.connect();
-*/
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
@@ -18,14 +13,8 @@ app.get('/Test', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-app.get('/userinfo/:id', account.UserInfo);
-/*
-app.get('/userinfo/:id', function(req, res) {
-	var id = req.params.id;
-	var sql = "SELECT name, identification_number__c, gender__c FROM salesforce.Account WHERE SFID='" + id + "'";
-	client.query(sql, function(error, data) {res.json(data.rows[0]);});
-})
-*/
+app.get('/userinfo/:id', account.UserInfobyId);
+app.get('/userinfo/:firstname/:lastname', account.UserInfobyName);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
--- server/account.js
+++ server/account.js
@@ -1,8 +1,19 @@
 var db = require('./pghelper');
 
-exports.UserInfo = function(req, res, next) {
+exports.UserInfobyId = function(req, res, next) {
 	var id = req.params.id;
 	db.select("SELECT * FROM salesforce.Account WHERE SFID='" + id + "'")
+	.then(function(results) {
+		console.log(results);	
+		res.json(results);
+	})
+    .catch(next);
+};
+
+exports.UserInfobyId = function(req, res, next) {
+	var first = req.params.firstname;
+	var last = req.params.lastname;
+	db.select("SELECT * FROM salesforce.Account WHERE firstname='" + first + "' and lastname='" + last + "'")
 	.then(function(results) {
 		console.log(results);	
 		res.json(results);
