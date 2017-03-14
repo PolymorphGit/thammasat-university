3645cc02827a0a7a19b812d9ea12b3579affc13f
iyingac <iyingac@gmail.com>
2017-03-14 13:39:43 +0700

    Test Node js

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ var express = require('express')
 //var bodyParser = require('body-parser'), compression = require('compression'), cors = require('cors');
 var	account = require('./server/account')
 var app = express()
+/*
 var pg = require('pg'),databaseURL = process.env.DATABASE_URL || 'postgres://localhost:5432/df3pgi81qfmoc7';
 var client = new pg.Client(databaseURL);
 client.connect();
-
+*/
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-//app.disable('etag');
-//app.use(cors());
-//app.use(bodyParser.json());
-//app.use(compression());
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
@@ -21,11 +18,14 @@ app.get('/Test', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
+app.get('/userinfo/:id', account.UserInfo);
+/*
 app.get('/userinfo/:id', function(req, res) {
 	var id = req.params.id;
 	var sql = "SELECT name, identification_number__c, gender__c FROM salesforce.Account WHERE SFID='" + id + "'";
 	client.query(sql, function(error, data) {res.json(data.rows[0]);});
 })
+*/
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
--- server/pghelper.js
+++ server/pghelper.js
@@ -1,29 +1,26 @@
 "use strict";
 
-let pg = require('pg'),
+var pg = require('pg'),
     //databaseURL = process.env.DATABASE_URL || 'postgres://ehuotalmpdqjvs:da48536ca63cdb9f209d7e00695d5e261441f7313b611d670bf104bbb1d24a5a@ec2-54-243-214-198.compute-1.amazonaws.com:5432/df3pgi81qfmoc7';
 	databaseURL = process.env.DATABASE_URL || 'postgres://localhost:5432/df3pgi81qfmoc7';
-
+/*
 if (process.env.DATABASE_URL !== undefined) 
 {
 	pg.defaults.ssl = true;	
 }
-
+*/
 var client = new pg.Client(databaseURL);
 client.connect();
 
 exports.select = function (sql) {
-	const results = [];
-	const query = client.query(sql, function (err, result) {
+	client.query(sql, function (err, result) {
     	if (err) throw err;
     	console.log(sql);
+    	console.log(result);
+    	
+    	return result;
     });
 	
-	query.on('row', (row) => {
-      results.push(row);
-    });
-	
-	return results;
 };
 
 exports.upsert = function (sql) {
