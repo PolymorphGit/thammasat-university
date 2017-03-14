3351dd8e3532c34ff80ebba1947b0ddfdafc686b
iyingac <iyingac@gmail.com>
2017-03-14 11:10:28 +0700

    Test Node js

--- server/account.js
+++ server/account.js
@@ -1,6 +1,6 @@
 var db = require('./pghelper');
 
-exports.UserInfo = (req, res, next) => {
+exports.UserInfo = function(req, res, next) {
 	let id = req.params.id;
 	db.query("SELECT * FROM salesforce.account WHERE SFID='" + id + "'");
 	res.json(db);
