554e64e18230b61dedc6868c64def3c8dccbe2ea
iyingac <iyingac@gmail.com>
2017-03-14 12:59:19 +0700

    Test Node js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var express = require('express')
-var app = express()
+var bodyParser = require('body-parser'), compression = require('compression'), cors = require('cors');
 var	account = require('./server/account')
+var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+//app.use(express.static(__dirname + '/public'))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(compression());
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
- 
 app.get('/Test', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
 app.get('/userinfo/:id', account.UserInfo);
 
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
