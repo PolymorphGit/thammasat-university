a6020bf705c7c0bdaccce2838cb498f7705e6bbb
iyingac <iyingac@gmail.com>
2017-03-14 10:32:39 +0700

    Test Node Js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var	account = require('./server/account')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -12,6 +13,9 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
  
-app.get('/Test', function(req, res) {
+api.get('/Test', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
+
+api.get('/userinfo', account.UserInfo);
+
---
+++ server/account.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var db = require('./pghelper');
+
+exports.UserInfo = (req, res, next) => {
+	let id = req.params.id;
+	db.query("SELECT * FROM salesforce.account WHERE SFID='" + id + "'");
+	res.json(db);
+};
