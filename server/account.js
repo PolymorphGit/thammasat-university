e56dcca56a2c8ad4dacfa49845c3312468651083
iyingac <iyingac@gmail.com>
2017-03-21 15:00:18 +0700

    Test HTTP Req

--- server/account.js
+++ server/account.js
@@ -8,13 +8,14 @@ exports.getInfo = function(req, res, next) {
 	  host: 'app64319644.auth0.com',
 	  path: '/userinfo',
 	  port: '443',
+	  method: 'GET',
 	  //This is the only line that is new. `headers` is an object with the headers to request
 	  Authorization: {'custom': head}
 	};
 	
 	callback = function(results) {
 		console.log(results);	
-		res.json(results);
+		//res.json(results);
 	}
 	
 	var httprequest = http.request(options, callback);
