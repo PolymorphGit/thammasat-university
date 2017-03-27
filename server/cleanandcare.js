a0a1dafaa4acfa4725c22e7088f8de4d14e839b7
iyingac <iyingac@gmail.com>
2017-03-27 10:24:55 +0700

    Submit Master Clean

--- server/cleanandcare.js
+++ server/cleanandcare.js
@@ -4,5 +4,12 @@ exports.getCleanRate = function(req, res, next) {
 	var head = req.headers['authorization'];
 	var https = require('https');
 	
+	db.select("SELECT * FROM salesforce.Master_Clean_Rate__c")
+	.then(function(results) {
+		console.log(results);	
+		res.json(results);
+	})
+    .catch(next);
+	
 	res.send("1");
 }
