9661255834b79c0a32517c6625169f42b09b789a
iyingac <iyingac@gmail.com>
2017-03-27 10:07:27 +0700

    Submit Room

--- server/Room.js
+++ server/Room.js
@@ -1,6 +1,6 @@
 var db = require('./pghelper');
 
-exports.getCleanRate = function(req, res, next) {
+exports.getInfo = function(req, res, next) {
 	var id = req.params.id;
 	db.select("SELECT * FROM salesforce.Product2 WHERE SFID='" + id + "'")
 	.then(function(results) {
