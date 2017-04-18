32491a5b437cf0a14c401150f2a7615b3da47d4f
iyingac <iyingac@gmail.com>
2017-04-18 14:14:11 +0700

    Submit Page

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/notification', noti.push);
 
 app.get('/view_account/:id', function(request, response){
 	var id = request.params.id;
-    response.sendFile(path.join(__dirname + '/view_account.html?id='+ id));
+    response.sendFile(path.join(__dirname + '/edit_account.html?id='+ id));
 });
 
 app.listen(app.get('port'), function() {
