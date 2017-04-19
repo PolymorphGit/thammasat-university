db201e2f7d00d0c23bedc61623571eabf0aad418
Chanagan <tonggun123@gmail.com>
2017-04-19 16:05:24 +0700

    Submit Page

--- public/javascript/select_roommate.js
+++ public/javascript/select_roommate.js
@@ -2,7 +2,8 @@ angular.module('accountApp', [])
 .controller('mainController', function ($scope, $http) {
 	
 	$scope.account = {
-			 
+		name : "abc",
+		identification_number__c : "1234"
 	};
 	
 });
