7603157cbabeb97ccce05cfd02e53b242cb6925a
Chanagan <tonggun123@gmail.com>
2017-04-20 10:18:57 +0700

    Submit Page

--- public/javascript/select_roommate.js
+++ public/javascript/select_roommate.js
@@ -2,8 +2,21 @@ angular.module('accountApp', [])
 .controller('mainController', function ($scope, $http) {
 	
 	$scope.account = {
-		name : "abc",
-		identification_number__c : "1234"
+		
+	}
+	
+	//Get a account Detail
+	$scope.getData = function (accountId) {	
+		alert(accountId);
+		$http.get('../userinfo/' + accountId)
+		.success((data) => {
+			console.log(data);
+			$scope.account = data[0];
+		})
+		.error((data) => {
+			console.log('Error: ' + data);
+		});
+	    
 	};
 	
 });
