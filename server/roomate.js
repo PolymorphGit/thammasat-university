b9bc34971b2ba42defafc03dfd75608ceb722b27
iyingac <iyingac@gmail.com>
2017-04-20 12:16:11 +0700

    Submit Roomate

--- server/roomate.js
+++ server/roomate.js
@@ -2,7 +2,7 @@ var db = require('./pghelper');
 
 exports.getAccountRoomate = function(req, res, next) {
 	var id = req.params.id;
-	db.select("SELECT * FROM salesforce.roomate__c WHERE primary_roommate__c='" + id + "'")
+	db.select("SELECT * FROM salesforce.roommate__c WHERE primary_roommate__c='" + id + "'")
 	.then(function(results) {
 		//console.log(results);
 		var listAcc = "";
@@ -30,7 +30,7 @@ exports.createRoomate = function(req, res, next) {
 	.then(function(results) {
 		if(result.length > 0)
 		{
-			db.select("INSERT INTO salesforce.Account (primary_roommate__c, co_roommate__c) VALUE ('" + p + "', '" + result[0].sfid + "')" )
+			db.select("INSERT INTO salesforce.roommate__c (primary_roommate__c, co_roommate__c) VALUE ('" + p + "', '" + result[0].sfid + "')" )
 			.then(function(results2) {
 				//console.log(results2);	
 				res.json(results2);
@@ -47,7 +47,7 @@ exports.createRoomate = function(req, res, next) {
 
 exports.deleteRoomate = function(req, res, next) {
 	var id = req.params.id;
-	db.select("DELETE FROM salesforce.Account WHERE SFID='" + id + "'" )
+	db.select("DELETE FROM salesforce.roommate__c WHERE SFID='" + id + "'" )
 	.then(function(results) {
 		//console.log(results);	
 		res.json(results);
@@ -64,7 +64,7 @@ exports.updateRoomate = function(req, res, next) {
 	.then(function(results) {
 		if(result.length > 0)
 		{
-			var query = "UPDATE salesforce.Account SET primary_roommate__c='" + p + "', "; 
+			var query = "UPDATE salesforce.roommate__c SET primary_roommate__c='" + p + "', "; 
 			query += "co_roommate__c='" + result[0].sfid + "' ";
 			query += " WHERE SFID='" + id + "'";
 			db.select(query)
