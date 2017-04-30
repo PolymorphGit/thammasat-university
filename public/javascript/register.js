f4d5799feb0482726afac427044b742eebc2b798
werapat <werapat.f@polymorph.co.th>
2017-04-30 19:38:12 +0700

    submit page
    add disabled__c 1

--- public/javascript/register.js
+++ public/javascript/register.js
@@ -83,6 +83,7 @@ angular.module('leadApp', [])
     	data[0].birthdate__c = data[0].birthdate__c == null || data[0].birthdate__c =='null' ? '' : data[0].birthdate__c;
     	data[0].personmobilephone = data[0].personmobilephone == null || data[0].personmobilephone =='null' ? '' : data[0].personmobilephone;
     	data[0].personemail = data[0].personemail == null || data[0].personemail =='null' ? '' : data[0].personemail;
+    	data[0].disabled__c = data[0].disabled__c == null || data[0].disabled__c =='null' ? '' : data[0].disabled__c;
     	data[0].congenital_disease__c = data[0].congenital_disease__c == null || data[0].congenital_disease__c =='null' ? '' : data[0].congenital_disease__c;
     	data[0].student_id__c = data[0].student_id__c == null || data[0].student_id__c =='null' ? '' : data[0].student_id__c;
     	data[0].faculty__c = data[0].faculty__c == null || data[0].faculty__c =='null' ? '' : data[0].faculty__c;
