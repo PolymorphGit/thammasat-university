76f5b11610f4283bba5d4cf8df452f9e0dc9a530
iyingac <iyingac@gmail.com>
2017-05-16 13:23:48 +0700

    Submit Select Roommate API

--- server/roommate.js
+++ server/roommate.js
@@ -32,7 +32,7 @@ exports.createRoommate = function(req, res, next) {
 	var p = req.headers['primary'];
 	var c = req.headers['co'];
 	
-	db.select("SELECT * FROM salesforce.Account WHERE (identification_number__c ='" + c + "' or passport_number__c = '" + c + "' or student_id__c='" + c + "') and secondary__c = false")
+	db.select("SELECT * FROM salesforce.Account WHERE (identification_number__c ='" + c + "' or passport_number__c = '" + c + "' or student_id__c='" + c + "') and secondary__c = false and room__c is not null")
 	.then(function(results) {
 		console.log(results);
 		console.log(p);
@@ -99,7 +99,7 @@ exports.updateRoommate = function(req, res, next) {
 	var p = req.headers['primary'];
 	var c = req.headers['co'];
 	
-	db.select("SELECT * FROM salesforce.Account WHERE (identification_number__c ='" + c + "' or passport_number__c = '" + c + "' or student_id__c='" + c + "') and secondary__c = false")
+	db.select("SELECT * FROM salesforce.Account WHERE (identification_number__c ='" + c + "' or passport_number__c = '" + c + "' or student_id__c='" + c + "') and secondary__c = false and room__c is not null")
 	.then(function(results) {
 		if(results.length > 0 && p != results[0].sfid)
 		{
