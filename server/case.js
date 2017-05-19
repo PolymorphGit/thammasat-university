b66fbc1133ad121ec32d3003c9b6ff6d7907a6e1
iyingac <iyingac@gmail.com>
2017-05-19 10:43:25 +0700

    Submit Open Case API

--- server/case.js
+++ server/case.js
@@ -126,7 +126,7 @@ exports.openCase = function(req, res, next) {
 				.then(function(results) {
 					db.select("SELECT * FROM salesforce.RecordType WHERE name='Problem'")
 					.then(function(results2) {
-						var query = "INSERT INTO salesforce.Case (recordtypeid, accountid, type, problem_sub_type__c, problem_type__c"
+						var query = "INSERT INTO salesforce.Case (recordtypeid, accountid, type, problem_type__c, problem_sub_type__c"
 						query += ", Description, allow_to_access_room__c, agree_to_pay__c, priority, subject) ";
 						query += "VALUES ('" + results2[0].sfid + "', '" + results[0].sfid + "', '" + req.body.type + "', '";
 						query += req.body.sub_type + "', '" + req.body.topic + "', '" + req.body.other + "', '" + req.body.access + "', '";
