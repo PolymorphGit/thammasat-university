7c66b321cea6936043f4f63e2f481470386afcb3
PolymorphGit <info.polymorph@gmail.com>
2017-06-29 16:15:52 +0700

    Update updateworkorder.js

--- updateworkorder.js
+++ updateworkorder.js
@@ -7,7 +7,7 @@ function updateWorkOrder() {
     query += "(SELECT sfid FROM salesforce.Case WHERE salesforce.Case.id = salesforce.WorderOrder.case_heroku_id__c) "
     query += "WHERE salesforce.WorkOrder.caseid i null RETURNING *";
     db.select(query)
-	  .then(function(results) {
+    .then(function(results) {
       if(results.length > 0)
       {
         for(var i = 0 ; i < results.length ; i++)
@@ -19,3 +19,17 @@ function updateWorkOrder() {
     
 }
 updateWorkOrder();
+
+function CheckNull(){
+	db.select("SELECT * FROM salesforce.WorkOrder SET caseid is null")	
+	.then(function(results) {
+	if(results.length > 0)
+	{
+		for(var i = 0 ; i < results.length ; i++)
+		{
+		  console.log('Update WorkOrder Id:' + results[i].id);
+		}
+	}
+	})
+}
+CheckNull();
