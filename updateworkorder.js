df5eb9ce4bea17bce7ccd74775ceb6772b627069
PolymorphGit <info.polymorph@gmail.com>
2017-06-29 15:30:32 +0700

    Create updateworkorder.js

---
+++ updateworkorder.js
@@ -0,0 +1,21 @@
+var db = require('./server/pghelper');
+var Pusher = require('pusher');
+
+function updateWorkOrder() {
+    //console.log('Notification');
+    query = "UPDATE salesforce.WorkOrder SET salesforce.WorkOrder.caseid = ";
+    query += "(SELECT sfid FROM salesforce.Case WHERE salesforce.Case.id = salesforce.WorderOrder.case_heroku_id__c) "
+    query += "WHERE salesforce.WorkOrder.caseid i null RETURNING *";
+    db.select("SELECT * FROM salesforce.WorkOrder WHERE caseid is null")
+	  .then(function(results) {
+      if(results.length > 0)
+      {
+        for(var i = 0 ; i < results.length ; i++)
+        {
+          console.log('Update WorkOrder Id:' + results[i].id);
+        }
+      }
+    })
+    
+}
+updateWorkOrder();
