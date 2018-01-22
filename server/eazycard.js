85ea4aa35bdf25e8bbfca137d5315d1e265b8942
PolymorphGit <info.polymorph@gmail.com>
2018-01-22 10:28:56 +0700

    Update eazycard.js

--- server/eazycard.js
+++ server/eazycard.js
@@ -1,9 +1,15 @@
 var db = require('./pghelper');
 
 exports.topupone2callprepare = function(req, res, next) {
-  res.send("Topup One2call Prepare");
+  var head = req.headers['authorization'];
+  var price = req.headers['price'];
+  
+  res.send("Topup One2call Prepare, Price:" + price);
 }
 
 exports.topupone2callconfirm = function(req, res, next) {
-  res.send("Topup One2call Confirm");
+  var head = req.headers['authorization'];
+  var otp = req.headers['otp'];
+  
+  res.send("Topup One2call Confirm, OTP:" + otp);
 }
